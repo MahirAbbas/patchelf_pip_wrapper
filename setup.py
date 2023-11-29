b198f8920bbc8eff82826517aea14322f5254753
mahir <mabbasest@mahirabbas.com>
2023-11-29 14:01:29 +0000

    new

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='patchelf_wrapper',
+    name='patchelf_pip_wrapper',
     version='0.1',
     packages=find_packages(),
     description='Python wrapper for patchelf',
