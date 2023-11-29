3f19f5773597e6faf84370bb9dc32d7fde49051e
mahir <mabbasest@mahirabbas.com>
2023-11-29 13:51:30 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='patchel_wrapper',
+    version='0.1',
+    packages=find_packages(),
+    description='Python wrapper for patchelf',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mahirabbas/patchelf_wrapper',
+    author='Mahir Abbas',
+    author_email='mabbasest@outlook.com',
+    license='MIT',
+    install_requires=[
+        # Any dependencies, if you have them
+    ],
+    classifiers=[
+        # Classifiers help categorize the project on PyPI
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
